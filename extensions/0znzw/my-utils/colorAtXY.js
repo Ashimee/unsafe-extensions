bbb4ad5365ee6aa265feae3939ee2adcb1635313
Surv <135030944+SurvExe1Pc@users.noreply.github.com>
2023-07-15 06:23:29 -0400

    moved some files and added niche toolbox

---
+++ extensions/0znzw/my-utils/colorAtXY.js
@@ -0,0 +1,67 @@
+  /*
+  * ColorAtXY extension v1.0 by 0znzw (English Version)
+  * Do not remove this comment
+  */
+  (function (Scratch) {
+    'use strict';
+
+    const BlockType = Scratch.BlockType;
+    const ArgumentType = Scratch.ArgumentType;
+    const Cast = Scratch.Cast;
+    const renderer = Scratch.vm.renderer;
+
+    class ColorAtXY {
+        getInfo() {
+            return {
+                id: '0znzwColorAtXY',
+                name: 'Color At XY',
+                blocks: [
+                    {
+                        opcode: 'getColorAt',
+                        text: 'get [attr] of color at x: [x] y: [y]',
+                        disableMonitor: true,
+                        blockType: BlockType.REPORTER,
+                        arguments: {
+                            attr: { type: ArgumentType.STRING, menu: 'modes' },
+                            x: { type: ArgumentType.NUMBER, defaultValue: 0 },
+                            y: { type: ArgumentType.NUMBER, defaultValue: 0 },
+                        }
+                    }
+                ],
+                menus: {
+                    modes: {
+                        acceptReporters: true,
+                        items: ['hex','json','rgba(json object)','red(r)','green(g)','blue(b)','alpha(a)']
+                    }
+                }
+            };
+        }
+
+        getColorAt(args) {
+          function rgbaToHex(t,r,a,S){let n=t.toString(16).padStart(2,"0"),o=r.toString(16).padStart(2,"0"),g=a.toString(16).padStart(2,"0"),i=S.toString(16).padStart(2,"0");return`#${n}${o}${g}${i}`}
+          var x = Cast.toNumber(args.x);
+          var y = Cast.toNumber(args.y);
+          const returnObj = renderer.extractColor(vm.runtime.stageWidth/2+x,vm.runtime.stageHeight/2+y,1).color;
+          switch (args.attr) {
+            case 'hex':
+              return rgbaToHex(returnObj.r,returnObj.g,returnObj.b,returnObj.a).toString();
+            case 'rgba(json object)':
+              return JSON.stringify(returnObj);
+            case 'json':
+              return JSON.stringify([returnObj.r,returnObj.g,returnObj.b,returnObj.a]);
+            case 'hex':
+              return 
+            case 'red(r)':
+              return returnObj.r;
+            case 'green(g)':
+              return returnObj.g;
+            case 'blue(b)':
+              return returnObj.b;
+            case 'alpha(a)':
+              return returnObj.a;
+          }
+        }
+    }
+
+    Scratch.extensions.register(new ColorAtXY());
+})(Scratch);
--- images/README.md
+++ images/README.md
@@ -3,6 +3,12 @@
 All images in this folder are licensed under the [GNU General Public License version 3](../licenses/GPL-3.0.txt). Please see the below list for additional attribution information.
 ## Placeholder.svg
  - Created by [Placeholder](#) in https://github.com/SurvExe1Pc/unsafe-extensions/issues/1#issuecomment-1
+
+## NicheToolbox.svg
+ - Created by [@Sharkpool](https://discord.com/channels/@me/1125293854187323444/1129657575873458216)
+ - Gear icon from https://svgsilh.com/image/2125614.html under [![PublicDomain](https://licensebuttons.net/p/zero/1.0/80x15.png)](https://creativecommons.org/publicdomain/zero/1.0/).
+ - Hammers icon from https://svgsilh.com/image/312334.html under [![PublicDomain](https://licensebuttons.net/p/zero/1.0/80x15.png)](https://creativecommons.org/publicdomain/zero/1.0/).
+ - For [@0znzw](https://scratch.mit.edu/users/0znzw)
 ## local-storage.svg
  - Created by [@True-Fantom](https://scratch.mit.edu/users/TrueFantom/) in https://github.com/TurboWarp/extensions/pull/498.
  - File icons based on https://icon-icons.com/icon/file-pdf/153412 under [CC BY 4.0](https://creativecommons.org/licenses/by/4.0/).
